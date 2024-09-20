ff2cc8e7995c1dff019c519cfb56f38d06d53ce3
huseinibnu <husein.mahmud03@gmail.com>
2024-09-20 11:24:41 +0700

    menambahkan logo usu

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ st.set_page_config(
 )
 
 st.header("Tahapan Tekstur MPASI\n", divider="green")
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
